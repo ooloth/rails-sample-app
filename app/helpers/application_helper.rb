26db64b126e7922c2ffdd36d989bbd2d8f180f5f
Michael Uloth <hello@michaeluloth.com>
2019-06-12 16:14:40 -0400

    Improve readability

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis
-  def full_title(page_title = '')
+  def full_title(page_title = '')                     # Method def, optional arg
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
-      base_title
+      base_title                                      # Implicit return
     else
       page_title + " | " + base_title
     end
